37efbe0312d42f225c03750664177a0f2d0748a2
ali <aliyoussef34@hotmail.fr>
2020-04-08 18:32:22 +0200

    working but shittily

--- src/store/auth.js
+++ src/store/auth.js
@@ -15,25 +15,20 @@ export default {
 	actions: {
 		async signIn({ dispatch }, credentials) {
 			let response = await axios.post('signin', credentials)
-			dispatch('attempt', response.data.csrf, credentials.email) // csrf = token 
+			console.log(response.data)
+			dispatch('attempt', response) // csrf = token 
 		},
-		async attempt({ commit }, token, email) {
-			commit('SET_TOKEN', token) //We set the token
-			
-			try {
-				const response = await axios.get('api/v1/me', { email } ,{
-					headers: {
-						Authorization: 'Bearer ' + token
-					}
-				})
-				console.log(response.data)
-				commit('SET_USER', response.data)
+		async attempt({ commit }, response) {
+			console.log(response.data.user)
+			commit('SET_TOKEN', response.data.csrf) 
+			commit('SET_USER', response.data.user )
+			window.location.href = 'dashboard'
+		},
 
-			} catch(e) {
-				commit('SET_USER', null)
-				commit('SET_TOKEN', null)
-			}
-		}
+		async signUp(_, credentials) {
+			let response =  await axios.post('signup', credentials)
+			console.log(response.data)
+			window.location.href = 'signin';		}
 	},
 
 	mutations: {
