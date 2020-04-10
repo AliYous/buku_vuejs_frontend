22362cf6219670b4ff4547ec73f781c04a1ad010
ali <aliyoussef34@hotmail.fr>
2020-04-10 12:10:16 +0200

    bookList filtering functionnal, will pass data as props to booklist component

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,51 +1,55 @@
 <template>
     <div>
         <div class="container-fluid"> 
-                <BookList v-bind:status="statusList[0]" v-bind:books="bookList"/>
-                <BookList v-bind:status="statusList[1]"/>
-                <BookList v-bind:status="statusList[2]"/>
+					<BookList v-bind:status="statusList[0]" v-bind:books="bookList"/>
+					<BookList v-bind:status="statusList[1]"/>
+					<BookList v-bind:status="statusList[2]"/>
         </div>
     </div>
 </template>
 
 <script>
 import BookList from '../components/books/BookList'
-import { mapActions } from 'vuex'
-
+import axios from 'axios'
 
 export default {
-    name: 'Dashboard',
-    components: {
-        BookList
-    },
-    data() {
-        return {
-        statusList: ["In Progress", "To Read", "Read"],
-        bookList: {},
-        readBooks: {},
-        }
-    },
-    created() {
-        this.bookList = this.getAllBooks()
-    },
+	name: 'Dashboard',
+	components: {
+		BookList
+	},
+	data() {
+		return {
+		statusList: ["In Progress", "To Read", "Read"],
+		bookList: [],
+		readBooks: [],
+		booksToRead: [],
+		currentlyReadingBooks: []
+		}
+	},
+	created() {
+		this.fetchAndFilterAllBooks()
+	},
 
-    methods: {
-        ...mapActions({
-            fetchBooks: 'fetchBooks'
-        }),
+	methods: {
+		async fetchAndFilterAllBooks() {
+			const response = await axios.get('api/v1/books');
+			const bookList = response.data
 
-        getAllBooks() {
-            this.fetchBooks()
-        }
-    } 
+		// Filtering books list based on book reading status
+			this.readBooks = bookList.filter(book => book.status === "read")
+			this.booksToRead = bookList.filter(book => book.status === "to_read")
+			this.currentlyReadingBooks = bookList.filter(book => book.status === "currently_reading")
+		}
+
+	} 
 }
 </script>
 
 <style>
-    .container-fluid {
-        display: flex;
-        flex-direction: row;
-        justify-content: space-around;
-        align-items: stretch;
-    }
+	.container-fluid {
+		display: flex;
+		flex-direction: row;
+		justify-content: space-around;
+		align-items: stretch;
+	}
 </style>
